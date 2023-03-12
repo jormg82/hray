496f6f866b3febfbfb50373643763d796c211b1d
Jorge L. Martin <jormg82@gmail.com>
2023-03-12 17:57:42 +0100

    Implementado el mapeo a Unit Disk

--- src/Sampler/Sampler.hs
+++ src/Sampler/Sampler.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE RecordWildCards #-}
 
 module Sampler.Sampler (
-  Sample, SampledUnitSquare,
-  Sampler(numSamples, numSets, sets),
+  Sample,
+  Sampler(..),
   SampleGenFn,
   generateSampler,
   sampleUnitSquare,
+  sampleUnitDisk,
   genRandomSample,
   genRegularSample
 ) where
@@ -16,32 +18,41 @@ import Data.Array
 import System.Random
 
 
-type Sample = Point2D
-type SampledUnitSquare = [Sample]
+type Sample       = Point2D
+type SampledShape = [Sample]
 
                        -- Num samples, num sets
 data Sampler = Sampler
   {
-    numSamples :: Int,
-    numSets    :: Int,
-    sets       :: Array Int SampledUnitSquare
+    numSamples     :: Int,
+    numSets        :: Int,
+    unitSquareSets :: Array Int SampledShape,
+    unitDiskSets   :: Array Int SampledShape
   }
   deriving Show
 
-type SampleGenFn = Int -> IO SampledUnitSquare
 
+type SampleGenFn = Int -> IO SampledShape
 
 generateSampler :: Int -- Number of samples
                 -> Int -- Number of sets
                 -> SampleGenFn
                 -> IO Sampler
 generateSampler samples sets gen = do
-  xs <- mapM gen (replicate sets samples)
-  return $ Sampler samples sets $ listArray (0, sets-1) xs
+  let rSamples = replicate sets samples
+  xs <- mapM gen rSamples
+  ys <- mapM ((map sampleToUnitDisk <$>) . gen) rSamples
+  let array1 = listArray (0, sets-1) xs
+      array2 = listArray (0, sets-1) ys
+  return $ Sampler samples sets array1 array2
 
 
-sampleUnitSquare :: Sampler -> IO SampledUnitSquare
-sampleUnitSquare (Sampler _ n arr) = (arr !) <$> randomRIO (0, n-1)
+sampleUnitSquare :: Sampler -> IO SampledShape
+sampleUnitSquare Sampler{..} = (unitSquareSets !) <$> randomRIO (0, numSets-1)
+
+
+sampleUnitDisk :: Sampler -> IO SampledShape
+sampleUnitDisk Sampler{..} = (unitDiskSets !) <$> randomRIO (0, numSets-1)
 
 
 genRandomSample :: Int  -- Number of divisions of x and y
@@ -65,4 +76,18 @@ genRegularSample n (p, q) = point2D ((q'+0.5)/n') ((p'+0.5)/n')
     n' = fromIntegral n
     p' = fromIntegral p
     q' = fromIntegral q
+
+
+sampleToUnitDisk :: Sample -> Sample
+sampleToUnitDisk p
+  | a == 0 || b == 0 = p 
+  | a > (-b)  = if a > b
+                then point2D (a2/den) (a*b/den)
+                else point2D (a*b/den) (b2/den)
+  | otherwise = if a < b
+                then point2D ((-a2)/den) ((-a)*b/den)
+                else point2D ((-a)*b/den) ((-b2)/den)
+  where
+    (a, b, a2, b2) = (2*(p&x)-1, 2*(p&y)-1, a*a, b*b)
+    den = sqrt $ a2 + b2
 
